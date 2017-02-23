da43847f93632d77bc759313f22ecca8208f2b5f
snlo <snloveydus@sina.com>
2017-02-23 14:43:38 +0800

    Initial commit

---
+++ SNSegmentViewController/SNSegmentViewController/SNSegmentViewController.m
@@ -0,0 +1,37 @@
+//
+//  SNSegmentViewController.m
+//  SNSegmentViewController
+//
+//  Created by sunDong on 2017/2/23.
+//  Copyright © 2017年 snlo. All rights reserved.
+//
+
+#import "SNSegmentViewController.h"
+
+@interface SNSegmentViewController ()
+
+@end
+
+@implementation SNSegmentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
